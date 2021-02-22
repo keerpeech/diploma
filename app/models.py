e017a5a314d389681ec6b93d85570883f3aeddcf
keerpeech <keerpeech@gmail.com>
2021-02-22 13:33:17 +0600

    jwt token and db templates

---
+++ app/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, PermissionsMixin, BaseUserManager
+)
+from __future__ import unicode_literals
+from django.utils import timezone
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('The given email must be set')
+        try:
+            with transaction.atomic():
+                user = self.model(email=email, **extra_fields)
+                user.set_password(password)
+                user.save(using=self._db)
+                return user
+        except:
+            raise
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(email, password=password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=40, unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+    def save(self, *args, **kwargs):
+        super(User, self).save(*args, **kwargs)
+        return self
+
+class Student():
+	Id = models.BigIntegerField(primary_key=True, null=False, unique=True)
+	groups = models.ManyToManyField(Group)
+
+class Teacher():
+	Id = models.BigIntegerField(primary_key=True, null=False, unique=True)
+
+class Lesson(models.Model):
+	Id = models.BigIntegerField(primary_key=True, null=False, unique=True)
+	course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+class Course():
+	Id = models.BigIntegerField(primary_key=True, null=False, unique=True)
+
+class Group():
+	Id = models.BigIntegerField(primary_key=True, null=False, unique=True)
+	course = models.ForeignKey(Course, on_delete=models.CASCADE)
+	teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
